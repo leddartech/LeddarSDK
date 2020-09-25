1f74ae0244c320e21f5ee848ffa67ba25b3134cb
Erlend E. Aasland <erlend.aasland@innova.no>
2020-09-25 15:21:37 +0200

    Blacken setup.py

--- src/LeddarPy/setup.py
+++ src/LeddarPy/setup.py
@@ -10,21 +10,26 @@ from distutils.core import Extension
 
 import numpy as np
 
-if platform.system() == 'Windows':
+if platform.system() == "Windows":
     extra_compile_args = []
-    library_dirs = ['../LeddarConfigurator4/x64/Release']
-    libraries = ['LeddarConfigurator4']
+    library_dirs = ["../LeddarConfigurator4/x64/Release"]
+    libraries = ["LeddarConfigurator4"]
 else:
-    extra_compile_args = ['-std=c++11', '-Wno-write-strings', '-Wno-unused-function', '-Wno-unused-variable']
-    library_dirs = ['../release']
-    libraries = ['LeddarConfigurator4','usb-1.0']
-    
-# define 'LEDDAR_DEBUG' using e.g. 
-# user@machine:~/LC4/main/src/LeddarPy$ (export LEDDAR_DEBUG=1; python setup.py install  --user)
-print("LEDDAR_DEBUG = " + str(os.environ.get('LEDDAR_DEBUG', 0)))
-debug = ['-g', '-DPy_DEBUG'] if os.environ.get('LEDDAR_DEBUG', 0) == "1" else []
+    extra_compile_args = [
+        "-std=c++11",
+        "-Wno-write-strings",
+        "-Wno-unused-function",
+        "-Wno-unused-variable",
+    ]
+    library_dirs = ["../release"]
+    libraries = ["LeddarConfigurator4", "usb-1.0"]
 
-# to debug, you can use 
+# define 'LEDDAR_DEBUG' using e.g.
+# user@machine:~/LC4/main/src/LeddarPy$ (export LEDDAR_DEBUG=1; python setup.py install  --user)
+print("LEDDAR_DEBUG = " + str(os.environ.get("LEDDAR_DEBUG", 0)))
+debug = ["-g", "-DPy_DEBUG"] if os.environ.get("LEDDAR_DEBUG", 0) == "1" else []
+
+# to debug, you can use
 # gdbserver localhost:1234 python-dbg my_script_that_imports_leddar.py
 
 # then, from another window:
@@ -33,7 +38,8 @@ debug = ['-g', '-DPy_DEBUG'] if os.environ.get('LEDDAR_DEBUG', 0) == "1" else []
 # (gdb) break LeddarPy.cpp:initleddar //FOR EXAMPLE
 
 # you can also connect from Eclipse
-# (you may want to add LC4/main/src as a "Makefile Project with Existing Code" and use a 'make' command inspired from the one found in ../build.sh)
+# (you may want to add LC4/main/src as a "Makefile Project with Existing Code"
+# and use a 'make' command inspired from the one found in ../build.sh)
 # C/C++ Remote Application
 # C/C++ Application : /usr/bin/python-dbg
 # "Select other" link -> "GDB (DSF) Manual Remote Debugguing Launcher"
@@ -41,16 +47,28 @@ debug = ['-g', '-DPy_DEBUG'] if os.environ.get('LEDDAR_DEBUG', 0) == "1" else []
 # Add a breakpoint! (e.g. in LeddarPy.cpp::initleddar())
 # Run!
 
-module1 = Extension('leddar',
-                    include_dirs = ['./','../Leddar','../LeddarTech','../shared/','../shared/comm','../../libs/RapidJson', np.get_include()],
-                    libraries = libraries,
-                    library_dirs = library_dirs,
-                    sources = ['./LeddarPy.cpp', './LeddarPyDevice.cpp', './Connecters.cpp'],
-                    extra_compile_args= extra_compile_args + debug)
+module1 = Extension(
+    "leddar",
+    include_dirs=[
+        "./",
+        "../Leddar",
+        "../LeddarTech",
+        "../shared/",
+        "../shared/comm",
+        "../../libs/RapidJson",
+        np.get_include(),
+    ],
+    libraries=libraries,
+    library_dirs=library_dirs,
+    sources=["./LeddarPy.cpp", "./LeddarPyDevice.cpp", "./Connecters.cpp"],
+    extra_compile_args=extra_compile_args + debug,
+)
 
-setup (name = 'leddar',
-       version = '1.0',
-       description = 'Python wrapper for LeddarTech SDK.',
-       author = 'David Levy, Maxime Lemonnier',
-       author_email = 'maxime.lemonnier@leddartech.com',
-       ext_modules = [module1])
+setup(
+    name="leddar",
+    version="1.0",
+    description="Python wrapper for LeddarTech SDK.",
+    author="David Levy, Maxime Lemonnier",
+    author_email="maxime.lemonnier@leddartech.com",
+    ext_modules=[module1],
+)
